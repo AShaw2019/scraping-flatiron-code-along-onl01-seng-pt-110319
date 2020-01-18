f2864714375f10e71a7a59a912f768422937963c
Ashley Twomey <aprier091012@gmail.com>
2020-01-18 15:18:05 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,6 +1,18 @@
 class Course
   attr_accessor :title, :schedule, :description
+  @@all = []
   
+  Def initialize
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+def self.reset_all
+  @@all.clear
+end
   
 end
 
